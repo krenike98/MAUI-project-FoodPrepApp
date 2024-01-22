7c2df1547aac679993c65f2ce037b8ed8da116ba
Kiss Renáta <kiss.reni1998@gmail.com>
2024-01-22 15:05:01 +0100

    Init Project

---
+++ FoodPrepApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FoodPrepApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ FoodprepApp.Persistence/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace FoodprepApp.Persistence
+{
+    // All the code in this file is included in all platforms.
+    public class Class1
+    {
+    }
+}
